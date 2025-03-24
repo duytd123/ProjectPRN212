e176264e111565604ab0eb8a81a08fbd96408b75
TranDucDuy-HE176416 <118241326+duytd123@users.noreply.github.com>
2025-03-24 12:10:03 +0700

    update 24/3

--- ProjectPRN212/LoginManagement.xaml.cs
+++ ProjectPRN212/LoginManagement.xaml.cs
@@ -3,58 +3,101 @@ using DataAccess.Models;
 using DataAccess.Repository;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ProjectPRN212
 {
-    /// <summary>
-    /// Interaction logic for LoginManagement.xaml
-    /// </summary>
     public partial class LoginManagement : Window
     {
-        private readonly UserObject _userObject;    
+        private readonly UserObject _userObject;
+        private List<KeyValuePair<DateTime, Tuple<int, string>>> _allLogs;
+
+        private int CurrentPage = 1;
+        private const int PageSize = 10;
+        private int TotalPages = 1;
 
         public LoginManagement()
         {
             InitializeComponent();
             _userObject = new UserObject();
             LoadSecurityLogs();
-        }      
+        }
+
         private void LoadSecurityLogs()
         {
-            
-            List<KeyValuePair<DateTime, Tuple<int, string>>> loginLogs = _userObject.GetLoginLogs();
+            _allLogs = _userObject.GetLoginLogs();
 
-            SecurityLogsGrid.ItemsSource = loginLogs.Select(log => new
+            TotalPages = (int)Math.Ceiling((double)_allLogs.Count / PageSize);
+            DisplayCurrentPage();
+        }
+
+        private void DisplayCurrentPage()
+        {
+            var currentLogs = _allLogs
+                .Skip((CurrentPage - 1) * PageSize)
+                .Take(PageSize)
+                .Select(log => new
+                {
+                    Timestamp = log.Key.ToString("g"),
+                    UserId = log.Value.Item1,
+                    Action = log.Value.Item2
+                }).ToList();
+
+            SecurityLogsGrid.ItemsSource = currentLogs;
+            UpdateButtonStates();
+        }
+
+        private void UpdateButtonStates()
+        {
+            btnFirst.IsEnabled = CurrentPage > 1;
+            btnPrevious.IsEnabled = CurrentPage > 1;
+            btnNext.IsEnabled = CurrentPage < TotalPages;
+            btnLast.IsEnabled = CurrentPage < TotalPages;
+        }
+
+        private void btnFirst_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage = 1;
+            DisplayCurrentPage();
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentPage > 1)
             {
-                Timestamp = log.Key.ToString("g"), 
-                UserId = log.Value.Item1,
-                Action = log.Value.Item2
-            }).ToList();
+                CurrentPage--;
+                DisplayCurrentPage();
+            }
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentPage < TotalPages)
+            {
+                CurrentPage++;
+                DisplayCurrentPage();
+            }
+        }
+
+        private void btnLast_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage = TotalPages;
+            DisplayCurrentPage();
         }
 
         private void btnBackToAdmin_Click(object sender, RoutedEventArgs e)
         {
-            AdminWindow adminWindow = new AdminWindow();
-            adminWindow.Left = this.Left;
-            adminWindow.Top = this.Top;
-            adminWindow.Width = this.Width;
-            adminWindow.Height = this.Height;
+            AdminWindow adminWindow = new AdminWindow
+            {
+                Left = this.Left,
+                Top = this.Top,
+                Width = this.Width,
+                Height = this.Height
+            };
             adminWindow.Show();
             this.Hide();
         }
-
-       
     }
 }
